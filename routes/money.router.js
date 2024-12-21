a9f87f54fafe692e4e9c0abb61d64da0ec4f9558
vuphuong1794 <phuongvu1794@gmail.com>
2024-12-21 11:01:33 +0700

    updates

--- routes/money.router.js
+++ routes/money.router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const { startSession } = require('mongoose');
 const money = require('../models/money.model');
 
 //tao ATM
@@ -13,9 +14,40 @@ router.post('/v1/api/user', async (req, res) => {
     }
 })
 
-//chuyen tien A -> B 
+//Chuyen tien A -> B 
 router.post('/v1/api/transfer', async (req, res) => {
+    const session = await startSession();
+    try {
+        const {fromId, toId, amount} = req.body;
+        //create startSession
+        session.startTransaction();
 
+        const amountFrom = await money.findOneAndUpdate({
+            userId: fromId,
+        }, {
+            $inc: {amount: -amount}
+        }, {session, new: true});
+
+        console.log(`Account ${fromId} has ${amountFrom}`);
+
+        const amountTo = await money.findOneAndUpdate({
+            userId: toId,
+        }, {
+            $inc: {amount: +amount}
+        }, {session, new: true});
+
+        console.log(`Account ${toId} has ${amountTo}`);
+
+        return res.json({
+            msg: 'Transfer success',
+        })
+    } catch (error) {
+        console.error('Error Transactions', error);
+        return res.json({
+            msg: 'Transfer fail',
+        })
+        
+    }
 })
 
 module.exports = router;
